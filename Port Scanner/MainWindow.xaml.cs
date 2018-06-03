4dfbaa92afad2b930f888ba5fb2956657c5670d8
SamScholefield1 <samscholefield1@hotmail.co.uk>
2018-06-03 21:44:24 +0100

    Works but is super slow.

--- Port Scanner/MainWindow.xaml.cs
+++ Port Scanner/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,34 @@ namespace Port_Scanner
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void buttonGo_Click(object sender, RoutedEventArgs e)
+        {
+            lbOpenPorts.Items.Clear();
+
+            int startPort = Convert.ToInt32(txtStartPort.Text);
+            int endPort = Convert.ToInt32(txtEndPort.Text);
+            int portRange = endPort - startPort + 1;
+            string IP = txtIP.Text;
+            ManualResetEvent[] doneEvents = new ManualResetEvent[portRange];
+            TCPScanner[] portResultArray = new TCPScanner[portRange];
+
+            for (int i = 0; i < portRange; i++)
+            {
+                doneEvents[i] = new ManualResetEvent(false);
+                TCPScanner f = new TCPScanner(startPort, endPort, IP, doneEvents[i]);
+                portResultArray[i] = f;
+                ThreadPool.SetMinThreads(200, 200);
+                ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, i);
+                startPort++;
+            }
+
+            for (int i = 0; i < portRange; i++)
+            {
+                TCPScanner f = portResultArray[i];
+                lbOpenPorts.Items.Add("Port: " + f.STARTPORT + " is " + f.portStatus);
+            }
         }
     }
 }
---
+++ Port Scanner/TCPScanner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Port_Scanner
+{
+    public class TCPScanner
+    {
+        private ManualResetEvent _doneEvent;
+        private int _startPort;
+        private int _endPort;
+        private int _port;
+        private string _IP;
+
+        public int STARTPORT { get { return _startPort; } }
+        public int ENDPORT { get { return _endPort; } }
+        public string portStatus { get { if(scanPort(_startPort)) { return "open"; } else { return "closed"; } } }
+
+        public TCPScanner(int startPort, int endPort, string IP, ManualResetEvent doneEvent)
+         {
+             _startPort = startPort;
+             _endPort = endPort;
+             _IP = IP;
+             _doneEvent = doneEvent;
+         }
+       
+         public void ThreadPoolCallback(Object threadContext)
+         {
+             int threadIndex = (int)threadContext;
+             Console.WriteLine("thread {0} started...", threadIndex);
+             bool portResult = scanPort(_startPort);
+             Console.WriteLine("thread {0} result calculated...", threadIndex);
+             _doneEvent.Set();
+         }
+
+         public bool scanPort(int startPort)
+         {
+             TcpClient tcp = new TcpClient();
+             bool connect;
+
+             try
+             {
+                 tcp.Connect(_IP, startPort);
+                 connect = true;
+             }
+             catch (SocketException)
+             {
+                 connect = false;
+             }
+             finally
+             {
+                 tcp.Close();
+             }
+             return connect;
+         } 
+    }
+}
